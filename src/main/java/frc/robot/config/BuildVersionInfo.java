bc92661fd340dd3193f5e68e9c70f03cfabcf1d2
Caleb Hagner <caleb.hagner@protonmail.com>
2022-02-18 22:54:08 -0500

    update wpilib build

--- src/main/java/frc/robot/config/BuildVersionInfo.java
+++ src/main/java/frc/robot/config/BuildVersionInfo.java
@@ -19,6 +19,6 @@ package frc.robot.config;
  **/
 class BuildVersionInfo {
     public static final String programmer = "Caleb Hagner";
-    public static final String commitSHA = "9988d36";
-    public static final String timeStamp = "20220217-203009";
+    public static final String commitSHA = "07b1412";
+    public static final String timeStamp = "20220218-224321";
 }
