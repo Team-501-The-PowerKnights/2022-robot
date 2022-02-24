81a549808217972e813ba19b1da1005892a172a3
Stu Lewin <java4first@thelewins.net>
2022-02-24 06:25:08 -0500

    Capture Final Configuration Files
    New dashboard configuration with new telemetry values. Not sure about form factor for other driver station laptops.

--- src/main/java/frc/robot/config/BuildVersionInfo.java
+++ src/main/java/frc/robot/config/BuildVersionInfo.java
@@ -19,6 +19,6 @@ package frc.robot.config;
  **/
 class BuildVersionInfo {
     public static final String programmer = "Stu Lewin";
-    public static final String commitSHA = "1b20fa2";
-    public static final String timeStamp = "20220219-155810";
+    public static final String commitSHA = "a6d7569";
+    public static final String timeStamp = "20220224-061707";
 }
