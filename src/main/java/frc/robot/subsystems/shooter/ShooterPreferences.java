218d65aa5dbfb4413c6edf523dc8026c740e97c9
Stu Lewin <java4first@thelewins.net>
2022-02-26 12:57:41 -0500

    Fix Typos in Subsystem Names
    Everything had the same name in definitions.

--- src/main/java/frc/robot/subsystems/shooter/ShooterPreferences.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterPreferences.java
@@ -34,7 +34,7 @@ public final class ShooterPreferences {
     /** Our classes' logger **/
     private static final PKLogger logger = RioLogger.getLogger(ShooterPreferences.class.getName());
 
-    static private final String name = SubsystemNames.driveName;
+    static private final String name = SubsystemNames.shooterName;
     static final String pid_P = name + ".P";
     static final String pid_I = name + ".I";
     static final String pid_D = name + ".D";
