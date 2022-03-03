257109fbf34c509be0999c0d342d9df52372d1d9
Caleb Hagner <caleb.hagner@protonmail.com>
2022-03-03 14:48:00 -0500

    Build

--- src/main/java/frc/robot/config/BuildVersionInfo.java
+++ src/main/java/frc/robot/config/BuildVersionInfo.java
@@ -19,6 +19,6 @@ package frc.robot.config;
  **/
 class BuildVersionInfo {
     public static final String programmer = "Caleb Hagner";
-    public static final String commitSHA = "eb256e3";
-    public static final String timeStamp = "20220303-143316";
+    public static final String commitSHA = "f3642c1";
+    public static final String timeStamp = "20220303-144748";
 }
