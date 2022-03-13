c92a1101ea038a153af1587fed3585fa81188ad3
Stu Lewin <java4first@thelewins.net>
2022-03-13 14:27:39 -0400

    Update Code Version
    Prep for Event #2 (so 2.0.0).

--- src/main/java/frc/robot/config/BuildVersionInfo.java
+++ src/main/java/frc/robot/config/BuildVersionInfo.java
@@ -19,6 +19,6 @@ package frc.robot.config;
  **/
 class BuildVersionInfo {
     public static final String programmer = "Stu Lewin";
-    public static final String commitSHA = "2670588";
-    public static final String timeStamp = "20220313-111504";
+    public static final String commitSHA = "019fe95";
+    public static final String timeStamp = "20220313-142702";
 }
