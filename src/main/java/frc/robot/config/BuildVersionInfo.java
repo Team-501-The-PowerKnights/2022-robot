30b59008053da853c4fe4d2d1f3913bf3c4170be
Stu Lewin <java4first@thelewins.net>
2022-03-26 18:41:10 -0400

    Change to Dash Display on Vision
    Open camera when not actively using alignment.

--- src/main/java/frc/robot/config/BuildVersionInfo.java
+++ src/main/java/frc/robot/config/BuildVersionInfo.java
@@ -19,6 +19,6 @@ package frc.robot.config;
  **/
 class BuildVersionInfo {
     public static final String programmer = "Stu Lewin";
-    public static final String commitSHA = "529413d";
-    public static final String timeStamp = "20220326-181714";
+    public static final String commitSHA = "b9107e5";
+    public static final String timeStamp = "20220326-183324";
 }
