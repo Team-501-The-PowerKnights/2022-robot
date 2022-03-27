cd5d687c2fca20787666d53fec810c7d491f65b3
Stu Lewin <java4first@thelewins.net>
2022-03-27 10:00:41 -0400

    Make Climbing Only Operator Enable
    Removed Driver 'anding'

--- src/main/java/frc/robot/config/BuildVersionInfo.java
+++ src/main/java/frc/robot/config/BuildVersionInfo.java
@@ -19,6 +19,6 @@ package frc.robot.config;
  **/
 class BuildVersionInfo {
     public static final String programmer = "Stu Lewin";
-    public static final String commitSHA = "b9107e5";
-    public static final String timeStamp = "20220326-183324";
+    public static final String commitSHA = "30b5900";
+    public static final String timeStamp = "20220327-095707";
 }
